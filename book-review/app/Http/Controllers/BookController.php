96a86d9cfe65da9b9b5132480d1ba7e585d4f23f
Arthur Foadjo <arthurfoadjo@yahoo.com>
2024-01-19 05:41:35 +0100

    Invalidating cache

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -53,13 +53,17 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
+        $cacheKey = "book" . $book->id;
+
+        $book = Cache::remember($cacheKey, 3600, fn() => $book->load(
+            [
+                "reviews" => fn($query) => $query->latest()
+            ]
+        ));
+
         return view(
             "books.show", 
-            ["book" => $book->load(
-                [
-                    "reviews" => fn ($query) => $query->latest()
-                ]
-            )]
+            ["book" => $book]
         );
     }
 
