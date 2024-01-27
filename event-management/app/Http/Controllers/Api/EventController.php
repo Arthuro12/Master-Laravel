f6a315a67184bd73318cf78fdf10dd8dd65a828a
Arthur Foadjo <arthurfoadjo@yahoo.com>
2024-01-27 16:47:27 +0100

    controlling json response

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Event;
+use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return \App\Models\Event::all();
+        return EventResource::collection(Event::with("user")->get());
     }
 
     /**
@@ -31,7 +32,7 @@ class EventController extends Controller
             "user_id" => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -39,7 +40,8 @@ class EventController extends Controller
      */
     public function show(\App\Models\Event $event)
     {
-        return $event;
+        $event->load("user", "attendees");
+        return new EventResource($event);
     }
 
     /**
@@ -57,7 +59,7 @@ class EventController extends Controller
             "user_id" => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
