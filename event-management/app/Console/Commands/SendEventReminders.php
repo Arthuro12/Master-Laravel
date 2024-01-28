6137b03d2ccd7d51f3f1ad35b740c6578ca26728
Arthur Foadjo <arthurfoadjo@yahoo.com>
2024-01-28 23:04:34 +0100

    Using customized command to implement system of notification

---
+++ event-management/app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sent notifications to all event attendees';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = \App\Models\Event::with("attendees.user")
+            ->whereBetween("start_time", [now(), now()->addDay()])
+            ->get();
+
+        $evenCount = $events->count();
+        $eventLabel = Str::plural("event", $evenCount);
+
+        $this->info("Found {$evenCount} {$eventLabel}.");
+
+        $events->each(fn ($event) => $event->attendees->each(
+            fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+        ));
+
+        $this->info("Reminder notifications sent succesfully!");
+    }
+}
