2bb5e69f70f1bb5a11252668079478e5acda1fca
Arthur Foadjo <arthurfoadjo@yahoo.com>
2024-01-28 23:43:34 +0100

    send notification and mails

--- event-management/app/Console/Commands/SendEventReminders.php
+++ event-management/app/Console/Commands/SendEventReminders.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -36,7 +37,11 @@ class SendEventReminders extends Command
         $this->info("Found {$evenCount} {$eventLabel}.");
 
         $events->each(fn ($event) => $event->attendees->each(
-            fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+            fn ($attendee) => $attendee->user->notify(
+                new EventReminderNotification(
+                    $event
+                )
+            )
         ));
 
         $this->info("Reminder notifications sent succesfully!");
